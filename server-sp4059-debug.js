d0f33140831e9a6744f1217763a9eac0617583c4
9163223-maker <9163223@gmail.com>
2026-05-09 01:44:21 +0300

    CC4.3 activate entry alias

--- server-sp4059-debug.js
+++ server-sp4059-debug.js
@@ -1,3 +1,3 @@
 'use strict';
-console.log('[CC4.2] entry alias: server-sp4059-debug.js -> server-cc42.js');
-require('./server-cc42.js');
+console.log('[CC4.3] entry alias: server-sp4059-debug.js -> server-cc43.js');
+require('./server-cc43.js');
