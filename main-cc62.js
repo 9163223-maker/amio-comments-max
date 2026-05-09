dea8f24807183bb3d1df4c81ad12ee1b6d1a194f
9163223-maker <9163223@gmail.com>
2026-05-09 23:59:45 +0300

    CC6.5.3 update main entry

--- main-cc62.js
+++ main-cc62.js
@@ -1,10 +1,8 @@
 'use strict';
-
-// CC6.5.2 entrypoint.
-// Keeps approved legacy comments UI and makes navigation callback ACKs silent.
-process.env.BUILD_VERSION = 'CC6.5.2';
-process.env.RUNTIME_VERSION = 'CC6.5.2';
-process.env.BUILD_SOURCE_MARKER = 'adminkit-CC6.5.2-silent-navigation-callbacks';
-
+process.env.BUILD_VERSION = 'CC6.5.3';
+process.env.RUNTIME_VERSION = 'CC6.5.3';
+process.env.BUILD_SOURCE_MARKER = 'adminkit-CC6.5.3-comments-router-guard-logo-fit';
+require('./cc653-qa-lite').install();
+require('./cc653-comments-router-guard-logo').install();
 require('./cc652-silent-callbacks').install();
 require('./cc5-bootstrap-lite');
