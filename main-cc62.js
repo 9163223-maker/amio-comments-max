7dc3bd3b37dad7ec42c9bbedbcd15a8e1b946434
9163223-maker <9163223@gmail.com>
2026-05-10 01:56:02 +0300

    CC6.5.5 enable optimized logo assets

--- main-cc62.js
+++ main-cc62.js
@@ -1,8 +1,8 @@
 'use strict';
-process.env.BUILD_VERSION = 'CC6.5.4';
-process.env.RUNTIME_VERSION = 'CC6.5.4';
-process.env.BUILD_SOURCE_MARKER = 'adminkit-CC6.5.4-optimized-logo-asset';
-require('./cc654-optimized-logo-asset').install();
+process.env.BUILD_VERSION = 'CC6.5.5';
+process.env.RUNTIME_VERSION = 'CC6.5.5';
+process.env.BUILD_SOURCE_MARKER = 'adminkit-CC6.5.5-logo-assets';
+require('./cc655-landing-logo-asset').install();
 require('./cc653-qa-lite').install();
 require('./cc653-comments-router-guard-logo').install();
 require('./cc652-silent-callbacks').install();
