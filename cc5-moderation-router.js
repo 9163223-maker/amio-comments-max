b65b11a5d134ad01f348ecd0886a44da01839b79
9163223-maker <9163223@gmail.com>
2026-05-09 12:02:18 +0300

    CC5.1.1 route adapter to clean post picker hotfix

--- cc5-moderation-router.js
+++ cc5-moderation-router.js
@@ -1,3 +1,3 @@
 'use strict';
-// CC5.1: single source router for moderation. Legacy file name is kept only as adapter.
-module.exports = require('./cc51-router');
+// CC5.1.1: legacy filename kept only as adapter to the clean hotfix router.
+module.exports = require('./cc51-router-hotfix');
