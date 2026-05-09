60c699de99f03c575a5168f4cb2e844458504c4f
9163223-maker <9163223@gmail.com>
2026-05-09 13:52:30 +0300

    CC5.4 set main runtime marker

--- main-cc5.js
+++ main-cc5.js
@@ -1,6 +1,6 @@
 'use strict';
-process.env.BUILD_VERSION='CC5.3';
-process.env.RUNTIME_VERSION='CC5.3';
-process.env.BUILD_SOURCE_MARKER='adminkit-CC5.3-comments-clean-shell';
+process.env.BUILD_VERSION='CC5.4';
+process.env.RUNTIME_VERSION='CC5.4';
+process.env.BUILD_SOURCE_MARKER='adminkit-CC5.4-comments-post-register-and-cta';
 require('./cc52-db-guard').install();
 require('./cc5-bootstrap-lite');
