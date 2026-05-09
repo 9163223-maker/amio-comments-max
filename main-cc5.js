074473df0ba02c14b89463b6a0165217044ea322
9163223-maker <9163223@gmail.com>
2026-05-09 12:23:48 +0300

    CC5.2 set main runtime marker

--- main-cc5.js
+++ main-cc5.js
@@ -1,5 +1,5 @@
 'use strict';
-process.env.BUILD_VERSION='CC5.0';
-process.env.RUNTIME_VERSION='CC5.0';
-process.env.BUILD_SOURCE_MARKER='adminkit-CC5-db-only-moderation-core';
+process.env.BUILD_VERSION='CC5.2';
+process.env.RUNTIME_VERSION='CC5.2';
+process.env.BUILD_SOURCE_MARKER='adminkit-CC5.2-clean-moderation-router';
 require('./cc5-bootstrap-lite');
