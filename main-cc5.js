01375f0c31e2a290166e9fc0442e7ad4c31d159b
9163223-maker <9163223@gmail.com>
2026-05-09 16:05:27 +0300

    CC6.0 bump entry runtime marker

--- main-cc5.js
+++ main-cc5.js
@@ -1,6 +1,6 @@
 'use strict';
-process.env.BUILD_VERSION='CC5.7';
-process.env.RUNTIME_VERSION='CC5.7';
-process.env.BUILD_SOURCE_MARKER='adminkit-CC5.7-comments-open-independent';
+process.env.BUILD_VERSION='CC6.0';
+process.env.RUNTIME_VERSION='CC6.0';
+process.env.BUILD_SOURCE_MARKER='adminkit-CC6.0-comments-standalone-clean-boot';
 require('./cc52-db-guard').install();
 require('./cc5-bootstrap-lite');
