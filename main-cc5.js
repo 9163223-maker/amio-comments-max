bdd7be380b399f181cf78bc38d4fc2d7119766c7
9163223-maker <9163223@gmail.com>
2026-05-09 12:29:08 +0300

    CC5.2 activate DB guard before bootstrap

--- main-cc5.js
+++ main-cc5.js
@@ -2,4 +2,5 @@
 process.env.BUILD_VERSION='CC5.2';
 process.env.RUNTIME_VERSION='CC5.2';
 process.env.BUILD_SOURCE_MARKER='adminkit-CC5.2-clean-moderation-router';
+require('./cc52-db-guard').install();
 require('./cc5-bootstrap-lite');
