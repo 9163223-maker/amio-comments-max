1727119e6572920a704e9a1a1a144c005b594e82
9163223-maker <9163223@gmail.com>
2026-05-09 10:30:03 +0300

    CC5 add active bootstrap server

---
+++ server-cc5.js
@@ -0,0 +1,5 @@
+'use strict';
+process.env.BUILD_VERSION='CC5.0';
+process.env.RUNTIME_VERSION='CC5.0';
+process.env.BUILD_SOURCE_MARKER='adminkit-CC5-db-only-moderation-core';
+require('./cc5-bootstrap');
