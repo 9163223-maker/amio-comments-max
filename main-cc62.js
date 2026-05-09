860fb91c3ea7e5c9be27d52641d7756f28c568a4
9163223-maker <9163223@gmail.com>
2026-05-09 20:12:01 +0300

    CC6.5 update entrypoint marker

--- main-cc62.js
+++ main-cc62.js
@@ -1,9 +1,9 @@
 'use strict';
 
-// CC6.4 entrypoint.
-// Bootstrap keeps approved legacy comments UI as /app owner and adds DB truth/debug routes.
-process.env.BUILD_VERSION = 'CC6.4';
-process.env.RUNTIME_VERSION = 'CC6.4';
-process.env.BUILD_SOURCE_MARKER = 'adminkit-CC6.4-moderation-db-truth';
+// CC6.5 entrypoint.
+// Bootstrap keeps approved legacy comments UI as /app owner and adds moderation DB truth/title repair.
+process.env.BUILD_VERSION = 'CC6.5';
+process.env.RUNTIME_VERSION = 'CC6.5';
+process.env.BUILD_SOURCE_MARKER = 'adminkit-CC6.5-moderation-title-repair';
 
 require('./cc5-bootstrap-lite');
