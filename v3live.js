bf5c8f7468dc34884790153071dd4b2192f5b1b6
9163223-maker <9163223@gmail.com>
2026-05-11 00:27:01 +0300

    Patch V3 adapter cache through safe shim

--- v3live.js
+++ v3live.js
@@ -1,2 +1,6 @@
-console.log('safe shim');
-exports.install = function(){ return { ok: true }; };
+'use strict';
+exports.install=function(){
+  const id=require.resolve('./menu-v3-feature-adapter');
+  require.cache[id]={id,filename:id,loaded:true,exports:require('./menu-v3-feature-adapter-fixed')};
+  return {ok:true,runtimeVersion:'CC6.5.5.2',sourceMarker:'adminkit-v3-fixed-cache-bridge'};
+};
