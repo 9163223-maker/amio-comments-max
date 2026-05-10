04a8831f9d3a286a3419fb6edbf74673e22dee92
9163223-maker <9163223@gmail.com>
2026-05-10 14:04:53 +0300

    CC6.5.2.2 keep persistence runtime marker last

--- main-cc6522.js
+++ main-cc6522.js
@@ -1,15 +1,15 @@
 'use strict';
 
+const menuReliability = require('./cc6521-start-logo-hotfix');
+const silentCallbacks = require('./cc652-silent-callbacks');
+const persistence = require('./cc6522-persistence-hotfix');
+
+menuReliability.install();
+silentCallbacks.install();
+persistence.install();
+
 process.env.BUILD_VERSION = 'CC6.5.2.2';
 process.env.RUNTIME_VERSION = 'CC6.5.2.2';
 process.env.BUILD_SOURCE_MARKER = 'adminkit-CC6.5.2.2-persistence-hotfix';
-
-const persistence = require('./cc6522-persistence-hotfix');
-const menuReliability = require('./cc6521-start-logo-hotfix');
-const silentCallbacks = require('./cc652-silent-callbacks');
-
-persistence.install();
-menuReliability.install();
-silentCallbacks.install();
 
 require('./cc5-bootstrap-lite');
