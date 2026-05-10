48ead4ff9b39d9d6fe503b20a9a16edb6caf0830
9163223-maker <9163223@gmail.com>
2026-05-10 14:53:54 +0300

    Wire CC6.5.2.4 production menu map

--- main-cc6522.js
+++ main-cc6522.js
@@ -1,15 +1,9 @@
 'use strict';
-
-const cleanMenu = require('./cc6523-clean-menu-router');
-const persistence = require('./cc6522-persistence-hotfix');
-const silentCallbacks = require('./cc652-silent-callbacks');
-
-cleanMenu.install();
-persistence.install();
-silentCallbacks.install();
-
-process.env.BUILD_VERSION = 'CC6.5.2.3';
-process.env.RUNTIME_VERSION = 'CC6.5.2.3';
-process.env.BUILD_SOURCE_MARKER = 'adminkit-CC6.5.2.3-clean-menu-router';
-
+require('./cc6524-production-menu-map-debug').install();
+require('./cc6523-clean-menu-router').install();
+require('./cc6522-persistence-hotfix').install();
+require('./cc652-silent-callbacks').install();
+process.env.BUILD_VERSION='CC6.5.2.4';
+process.env.RUNTIME_VERSION='CC6.5.2.4';
+process.env.BUILD_SOURCE_MARKER='adminkit-CC6.5.2.4-production-menu-map';
 require('./cc5-bootstrap-lite');
