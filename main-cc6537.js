1dd2e75d8565be3f3cc6bc40fb47820853d96c19
9163223-maker <9163223@gmail.com>
2026-05-10 19:30:22 +0300

    Enable Production Menu V3 renderer before legacy handlers

--- main-cc6537.js
+++ main-cc6537.js
@@ -1,5 +1,6 @@
 'use strict';
 
+require('./production-menu-v3-renderer').install();
 require('./production-menu-map-v3-fixed-debug').install();
 require('./cc6542-hotfix-router').install();
 require('./cc5-bootstrap-lite');
