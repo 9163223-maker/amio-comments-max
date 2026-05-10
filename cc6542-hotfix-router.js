fa0afdf117a5b72443da59d19c24c1a177925415
9163223-maker <9163223@gmail.com>
2026-05-11 01:43:00 +0300

    restore minimal hotfix shim

---
+++ cc6542-hotfix-router.js
@@ -0,0 +1,4 @@
+'use strict';
+exports.install=function(){return {ok:true,runtimeVersion:'CC6.5.5.1-HOTFIX-SHIM',sourceMarker:'adminkit-hotfix-shim-restored',retired:true};};
+exports.selfTest=exports.install;
+exports.handleUpdate=async function(){return false;};
