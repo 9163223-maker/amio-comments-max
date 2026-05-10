55f6e6be2d2bbd1b0adb9f08d4f63774c84a67b0
9163223-maker <9163223@gmail.com>
2026-05-10 15:43:58 +0300

    Wire CC6.5.2.8 access status route before channel and legacy layers

--- main-cc6526.js
+++ main-cc6526.js
@@ -1,10 +1,11 @@
 'use strict';
+require('./cc6528-access-status-route').install();
 require('./cc6527-channel-access-check').install();
 require('./cc6526-production-test-ui').install();
 require('./cc6524-production-menu-map-debug').install();
 require('./cc6522-persistence-hotfix').install();
 require('./cc652-silent-callbacks').install();
-process.env.BUILD_VERSION='CC6.5.2.7';
-process.env.RUNTIME_VERSION='CC6.5.2.7';
-process.env.BUILD_SOURCE_MARKER='adminkit-CC6.5.2.7-channel-access-check';
+process.env.BUILD_VERSION='CC6.5.2.8';
+process.env.RUNTIME_VERSION='CC6.5.2.8';
+process.env.BUILD_SOURCE_MARKER='adminkit-CC6.5.2.8-access-status-route';
 require('./cc5-bootstrap-lite');
