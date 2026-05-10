cf231cb8e03d98519bc93f51436d5133e589286f
9163223-maker <9163223@gmail.com>
2026-05-10 15:08:11 +0300

    Wire CC6.5.2.5 production menu runtime

--- main-cc6522.js
+++ main-cc6522.js
@@ -1,9 +1,9 @@
 'use strict';
+require('./cc6525-production-menu-runtime').install();
 require('./cc6524-production-menu-map-debug').install();
-require('./cc6523-clean-menu-router').install();
 require('./cc6522-persistence-hotfix').install();
 require('./cc652-silent-callbacks').install();
-process.env.BUILD_VERSION='CC6.5.2.4';
-process.env.RUNTIME_VERSION='CC6.5.2.4';
-process.env.BUILD_SOURCE_MARKER='adminkit-CC6.5.2.4-production-menu-map';
+process.env.BUILD_VERSION='CC6.5.2.5';
+process.env.RUNTIME_VERSION='CC6.5.2.5';
+process.env.BUILD_SOURCE_MARKER='adminkit-CC6.5.2.5-production-menu-runtime';
 require('./cc5-bootstrap-lite');
