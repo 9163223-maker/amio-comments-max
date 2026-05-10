2df3cb1500a5d10fb11397643e35d62a7175c84f
9163223-maker <9163223@gmail.com>
2026-05-10 19:13:16 +0300

    Use fixed Production Menu Map V3 debug layer

--- main-cc6537.js
+++ main-cc6537.js
@@ -1,5 +1,5 @@
 'use strict';
 
-require('./production-menu-map-v3-debug').install();
+require('./production-menu-map-v3-fixed-debug').install();
 require('./cc6542-hotfix-router').install();
 require('./cc5-bootstrap-lite');
