9dbf4eaafa00cd89bdb3447c6cd3bffb5a248cab
9163223-maker <9163223@gmail.com>
2026-05-10 22:35:34 +0300

    Add start v3 entry

---
+++ start-v3.js
@@ -0,0 +1,5 @@
+require('./v3live').install()
+require('./production-menu-v3-renderer-v2').install()
+require('./production-menu-map-v3-fixed-debug').install()
+require('./cc6542-hotfix-router').install()
+require('./cc5-bootstrap-lite')
