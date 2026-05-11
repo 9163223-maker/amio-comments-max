d6922ef70917535937c52b42075dd663d1b07b3f
9163223-maker <9163223@gmail.com>
2026-05-11 11:52:29 +0300

    Install silent callbacks and direct comments launch before menu boot

--- main-cc6537.js
+++ main-cc6537.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('./v3-silent-menu-callbacks').install();
+require('./v3-comments-launch-fix').install();
 require('./production-menu-v3-renderer-v2').install();
 require('./production-menu-map-v3-fixed-debug').install();
 require('./cc6542-hotfix-router').install();
