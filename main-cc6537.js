e05c25ca5435a87c64f4ed3958e95b914f2fa380
9163223-maker <9163223@gmail.com>
2026-05-11 19:42:54 +0300

    Install isolated safe comments launcher before server boot

--- main-cc6537.js
+++ main-cc6537.js
@@ -1,5 +1,6 @@
 'use strict';
 
+require('./adminkit-comments-safe-launcher').install();
 require('./adminkit-safe-launch-core').install();
 require('./v3-silent-menu-callbacks').install();
 require('./v3-comments-launch-fix').install();
