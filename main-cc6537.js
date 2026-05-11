93e41fc68c0c1417487f46195ab649b32d4c2ecc
9163223-maker <9163223@gmail.com>
2026-05-11 11:22:18 +0300

    Install V3 native hints cleanup before bootstrap

--- main-cc6537.js
+++ main-cc6537.js
@@ -3,4 +3,5 @@
 require('./production-menu-v3-renderer-v2').install();
 require('./production-menu-map-v3-fixed-debug').install();
 require('./cc6542-hotfix-router').install();
+require('./v3-native-hints-cleanup').install();
 require('./cc5-bootstrap-lite');
