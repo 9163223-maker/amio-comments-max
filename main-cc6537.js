f10447cb244b322cee351536d26624d41439bfd7
9163223-maker <9163223@gmail.com>
2026-05-11 13:35:37 +0300

    Install Clean V3 menu debug endpoints

--- main-cc6537.js
+++ main-cc6537.js
@@ -4,6 +4,7 @@ require('./v3-silent-menu-callbacks').install();
 require('./v3-comments-launch-fix').install();
 require('./v3-repatch-comments-links').install();
 require('./v3-register-post-debug').install();
+require('./clean-v3-menu-debug').install();
 require('./production-menu-v3-renderer-v2').install();
 require('./production-menu-map-v3-fixed-debug').install();
 require('./cc6542-hotfix-router').install();
