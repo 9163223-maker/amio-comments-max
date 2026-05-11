12b7324fedb7b4a142881d32f9c43161118fbc52
9163223-maker <9163223@gmail.com>
2026-05-11 20:49:17 +0300

    Install safe core parser before appjs launch patches

--- main-cc6537.js
+++ main-cc6537.js
@@ -1,5 +1,6 @@
 'use strict';
 
+require('./adminkit-comments-core-parser-patch').install();
 require('./adminkit-comments-appjs-file-patch').install();
 require('./adminkit-comments-appjs-route').install();
 require('./adminkit-comments-safe-launcher').install();
