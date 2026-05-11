2eaf2570821583fb48b22cfdd683f82623793f76
9163223-maker <9163223@gmail.com>
2026-05-11 14:05:51 +0300

    Install ultra short Clean V3 menu OK endpoint

--- main-cc6537.js
+++ main-cc6537.js
@@ -6,6 +6,7 @@ require('./v3-repatch-comments-links').install();
 require('./v3-register-post-debug').install();
 require('./clean-v3-menu-normalizer').install();
 require('./clean-v3-menu-debug').install();
+require('./clean-v3-menu-ok').install();
 require('./production-menu-v3-renderer-v2').install();
 require('./production-menu-map-v3-fixed-debug').install();
 require('./cc6542-hotfix-router').install();
