3392d6ad085f70e45e305717f55e1fbd9abf8b3b
9163223-maker <9163223@gmail.com>
2026-05-11 12:13:09 +0300

    Install comments link repatch endpoint before app boot

--- main-cc6537.js
+++ main-cc6537.js
@@ -2,6 +2,7 @@
 
 require('./v3-silent-menu-callbacks').install();
 require('./v3-comments-launch-fix').install();
+require('./v3-repatch-comments-links').install();
 require('./production-menu-v3-renderer-v2').install();
 require('./production-menu-map-v3-fixed-debug').install();
 require('./cc6542-hotfix-router').install();
