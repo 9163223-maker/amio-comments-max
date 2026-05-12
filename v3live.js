fcc3d7d706cbd857a7ad8d5b2a7b68ff0be0bb8c
9163223-maker <9163223@gmail.com>
2026-05-12 18:22:50 +0300

    Load V3 menu callback hard router

--- v3live.js
+++ v3live.js
@@ -2,5 +2,7 @@
 exports.install=function(){
   const id=require.resolve('./menu-v3-feature-adapter');
   require.cache[id]={id,filename:id,loaded:true,exports:require('./menu-v3-feature-adapter-fixed')};
-  return {ok:true,runtimeVersion:'CC6.5.5.2',sourceMarker:'adminkit-v3-fixed-cache-bridge'};
+  let hardRouter=null;
+  try { hardRouter=require('./v3-menu-callback-hard-router').install(); } catch (error) { hardRouter={ok:false,error:error&&error.message?error.message:String(error)}; }
+  return {ok:true,runtimeVersion:'CC6.6.7',sourceMarker:'adminkit-v3-fixed-cache-bridge-plus-hard-callback-router',hardRouter};
 };
