976c37850ede1eb7d9eeec940e541fa372dba553
9163223-maker <9163223@gmail.com>
2026-05-16 13:26:00 +0300

    Normalize Core lead magnets section id

--- src/modules/leadMagnets.js
+++ src/modules/leadMagnets.js
@@ -1,3 +1,11 @@
 'use strict';
 const { makeSection } = require('./_sectionFactory');
-module.exports = makeSection({ id: 'leadMagnets', title: 'Подарки / Лид-магниты', shortTitle: 'Лид-магниты', icon: '🎁', order: 40, feature: 'lead_magnets.enabled', description: 'Лид-магниты будут перенесены на ak_post_lead_magnets и ak_lead_magnet_conditions. Условия будут проверяться через accessManager.' });
+module.exports = makeSection({
+  id: 'lead_magnets',
+  title: 'Подарки / Лид-магниты',
+  shortTitle: 'Лид-магниты',
+  icon: '🎁',
+  order: 40,
+  feature: 'lead_magnets.enabled',
+  description: 'Лид-магниты будут перенесены на ak_post_lead_magnets и ak_lead_magnet_conditions. Условия будут проверяться через accessManager.'
+});
