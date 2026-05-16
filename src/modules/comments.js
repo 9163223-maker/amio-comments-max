b6d380cbb748631d1b19e06557d5593d02cebb56
9163223-maker <9163223@gmail.com>
2026-05-16 12:43:24 +0300

    Add core comments module file

---
+++ src/modules/comments.js
@@ -0,0 +1,3 @@
+'use strict';
+const { makeSection } = require('./_sectionFactory');
+module.exports = makeSection({ id: 'comments', title: 'Комментарии', icon: '💬', order: 20, feature: 'comments.enabled', description: 'Рабочее ядро комментариев переносим аккуратно.' });
