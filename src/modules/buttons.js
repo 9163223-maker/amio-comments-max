58c06876b9161b460efcd45add36c6c4d91a0f5f
9163223-maker <9163223@gmail.com>
2026-05-16 12:44:12 +0300

    Add core buttons module file

---
+++ src/modules/buttons.js
@@ -0,0 +1,3 @@
+'use strict';
+const { makeSection } = require('./_sectionFactory');
+module.exports = makeSection({ id: 'buttons', title: 'Кнопки', icon: '🔘', order: 30, feature: 'buttons.enabled', description: 'CTA-кнопки будут перенесены на чистую таблицу ak_post_buttons и единый postAddonManager.' });
