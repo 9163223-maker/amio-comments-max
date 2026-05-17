5e9a4d82f8bcdc49df91329b8a49bec4bfbae9e8
9163223-maker <9163223@gmail.com>
2026-05-17 11:58:01 +0300

    ADMINKIT-CORE-1.32 register all 15 menu sections

--- src/core/sectionRegistry.js
+++ src/core/sectionRegistry.js
@@ -3,12 +3,38 @@
 const modules = [
   require('../modules/channels'),
   require('../modules/comments'),
-  require('../modules/buttons'),
+  require('../modules/photoComments'),
+  require('../modules/reactionsReplies'),
   require('../modules/leadMagnets'),
+  require('../modules/buttons'),
+  require('../modules/postHighlights'),
+  require('../modules/polls'),
+  require('../modules/postEditor'),
   require('../modules/moderation'),
-  require('../modules/archive'),
   require('../modules/stats'),
-  require('../modules/settings')
+  require('../modules/navigation'),
+  require('../modules/startLanding'),
+  require('../modules/debugDiagnostics'),
+  require('../modules/productionChecklist')
+];
+
+const RUNTIME = 'ADMINKIT-CORE-SECTION-REGISTRY-1.32-FULL-15-SECTIONS';
+const REQUIRED_SECTION_IDS = [
+  'channels',
+  'comments',
+  'photo_comments',
+  'reactions_replies',
+  'lead_magnets',
+  'buttons',
+  'post_highlights',
+  'polls',
+  'post_editor',
+  'moderation',
+  'stats',
+  'navigation',
+  'start_landing',
+  'debug_diagnostics',
+  'production_checklist'
 ];
 
 function listAll() {
@@ -28,4 +54,21 @@ function routeMap() {
   return map;
 }
 
-module.exports = { listAll, find, routeMap };
+function selfTest() {
+  const sections = listAll();
+  const ids = sections.map((section) => section.id);
+  const missing = REQUIRED_SECTION_IDS.filter((id) => !ids.includes(id));
+  const duplicates = ids.filter((id, index) => ids.indexOf(id) !== index);
+  return {
+    ok: missing.length === 0 && duplicates.length === 0 && sections.length === 15,
+    runtimeVersion: RUNTIME,
+    sectionCount: sections.length,
+    requiredSectionCount: REQUIRED_SECTION_IDS.length,
+    ids,
+    missing,
+    duplicates,
+    fullMenuScaffoldReady: true
+  };
+}
+
+module.exports = { RUNTIME, REQUIRED_SECTION_IDS, listAll, find, routeMap, selfTest };
