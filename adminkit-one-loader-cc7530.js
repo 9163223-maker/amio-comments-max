a114fa7dcc7dc543784c3b4bdba66cf851bf4db7
9163223-maker <9163223@gmail.com>
2026-05-18 20:09:13 +0300

    Add CC7.5.30 loader for Core 1.47.0 channel connection

---
+++ adminkit-one-loader-cc7530.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const RUNTIME = 'CC7.5.30-CORE-1.47.0-CHANNEL-CONNECTION';
+const SOURCE = 'adminkit-cc7-5-30-core-1-47-0-channel-connection';
+const MARKER = '__ADMINKIT_CC7_5_30_CORE_1_47_0_CHANNEL_CONNECTION_LOADER__';
+const CANONICAL_PUBLIC_BASE_URL = 'https://p01--amio-commnets-max--qkpwxnxqqrnw.code.run';
+
+process.env.BUILD_VERSION = RUNTIME;
+process.env.RUNTIME_VERSION = RUNTIME;
+process.env.BUILD_SOURCE_MARKER = SOURCE;
+if (!process.env.ADMINKIT_PUBLIC_BASE_URL || /amio-comments-max/i.test(String(process.env.ADMINKIT_PUBLIC_BASE_URL))) {
+  process.env.ADMINKIT_PUBLIC_BASE_URL = CANONICAL_PUBLIC_BASE_URL;
+}
+
+if (!global[MARKER]) {
+  global[MARKER] = true;
+  require('./adminkit-one-loader-cc7529');
+  process.env.BUILD_VERSION = RUNTIME;
+  process.env.RUNTIME_VERSION = RUNTIME;
+  process.env.BUILD_SOURCE_MARKER = SOURCE;
+  if (!process.env.ADMINKIT_PUBLIC_BASE_URL || /amio-comments-max/i.test(String(process.env.ADMINKIT_PUBLIC_BASE_URL))) {
+    process.env.ADMINKIT_PUBLIC_BASE_URL = CANONICAL_PUBLIC_BASE_URL;
+  }
+}
+
+module.exports = { RUNTIME, SOURCE, MARKER, CANONICAL_PUBLIC_BASE_URL };
