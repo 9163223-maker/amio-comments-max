f1c10236c63c6aa88b2eb5799457f266332d755d
9163223-maker <9163223@gmail.com>
2026-05-18 21:57:49 +0300

    Switch stress test entrypoint to Start Landing 1.49.1

--- src/core/coreStressTest.js
+++ src/core/coreStressTest.js
@@ -1,4 +1,4 @@
 'use strict';
 
-const stressTest = require('./coreStressTestV16');
-module.exports = stressTest;
+const nextStressTest = require('./coreStressTestV17');
+module.exports = nextStressTest;
