b1af89341766e52a032a840353d7fb98366bfb6a
9163223-maker <9163223@gmail.com>
2026-05-18 18:34:41 +0300

    Add CC7.5.27 loader for Core 1.44.0 post editor archive

---
+++ adminkit-one-loader-cc7527.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const RUNTIME = 'CC7.5.27-CORE-1.44.0-POST-EDITOR-DIRECT-ARCHIVE';
+const SOURCE = 'adminkit-cc7-5-27-core-1-44-0-post-editor-direct-archive';
+const MARKER = '__ADMINKIT_CC7_5_27_CORE_1_44_0_POST_EDITOR_DIRECT_ARCHIVE_LOADER__';
+const CANONICAL_PUBLIC_BASE_URL = 'https://p01--amio-commnets-max--qkpwxnxqqrnw.code.run';
+
+process.env.BUILD_VERSION = RUNTIME;
+process.env.RUNTIME_VERSION = RUNTIME;
+process.env.BUILD_SOURCE_MARKER = SOURCE;
+if (!process.env.ADMINKIT_PUBLIC_BASE_URL || /amio-comments-max/i.test(String(process.env.ADMINKIT_PUBLIC_BASE_URL))) {
+  process.env.ADMINKIT_PUBLIC_BASE_URL = CANONICAL_PUBLIC_BASE_URL;
+}
+
+if (!global[MARKER]) {
+  global[MARKER] = true;
+  require('./adminkit-one-loader-cc7526');
+  process.env.BUILD_VERSION = RUNTIME;
+  process.env.RUNTIME_VERSION = RUNTIME;
+  process.env.BUILD_SOURCE_MARKER = SOURCE;
+  if (!process.env.ADMINKIT_PUBLIC_BASE_URL || /amio-comments-max/i.test(String(process.env.ADMINKIT_PUBLIC_BASE_URL))) {
+    process.env.ADMINKIT_PUBLIC_BASE_URL = CANONICAL_PUBLIC_BASE_URL;
+  }
+}
+
+module.exports = { RUNTIME, SOURCE, MARKER, CANONICAL_PUBLIC_BASE_URL };
