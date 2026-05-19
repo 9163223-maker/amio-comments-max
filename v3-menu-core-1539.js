8e6d9a32b92be66abcd895ed8986df5cd35bf7f3
9163223-maker <9163223@gmail.com>
2026-05-19 17:24:04 +0300

    Activate self-contained V3 menu core via 1539 proxy

--- v3-menu-core-1539.js
+++ v3-menu-core-1539.js
@@ -1,6 +1,3 @@
 'use strict';
-const base=require('./v3-menu-core-1538');
-const RUNTIME='CC7.5.34-CORE-1.53.9-V3-CALLBACK-TRACE';
-const SOURCE='adminkit-cc7-5-34-core-1-53-9-v3-callback-trace';
-function runtimeVersion(){return process.env.RUNTIME_VERSION||process.env.BUILD_VERSION||RUNTIME;}
-module.exports={...base,RUNTIME,SOURCE,runtimeVersion};
+
+module.exports = require('./v3-menu-core-1540');
