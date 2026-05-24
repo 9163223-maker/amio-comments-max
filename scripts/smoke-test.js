e32a119265a03343cf61f6272ad61330320ff75c
9163223-maker <9163223@gmail.com>
2026-05-24 18:23:36 +0300

    PR69: wire fast comment send smoke test

--- scripts/smoke-test.js
+++ scripts/smoke-test.js
@@ -37,5 +37,6 @@ assert.strictEqual(postPatcher.PATCH_COALESCE_RUNTIME, 'CC8.1.10-PATCH-REPATCH-C
 runSmoke('timing-menu-audit-test.js', 'timing/menu audit smoke test');
 runSmoke('comment-skeleton-consumer-pr67-test.js', 'comment skeleton consumer PR67 smoke test');
 runSmoke('patch-coalescing-pr68-test.js', 'patch coalescing PR68 smoke test');
+runSmoke('fast-comment-send-pr69-test.js', 'fast comment send PR69 smoke test');
 
 console.log('smoke ok');
